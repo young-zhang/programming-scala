5c128afd82f54dd0be1185c05d7e97df706d42aa
Young Zhang <young.zhang@credit-suisse.com>
2014-01-07 17:46:04 -0500

    Modified button-clickable-observer-vetoable2-spec.scala

--- ch06/build.sbt
+++ ch06/build.sbt
@@ -1,4 +1,4 @@
-name := "hello"
+name := "Chapter 6"
 
 version := "1.0"
 
--- ch06/src/main/scala/ui3/button-clickable-observer-vetoable2-spec.scala
+++ ch06/src/main/scala/ui3/button-clickable-observer-vetoable2-spec.scala
@@ -5,14 +5,14 @@ import ui.ButtonCountObserver
 
 object ButtonClickableObserverVetoableSpec2 {
   def main(args: Array[String]): Unit = {
-    val observableButton =
-      new Button("Okay") with ObservableClicks with VetoableClicks {
-        maxAllowed = 2
-      }
-//    observableButton.maxAllowed mustEqual 2
-//    val buttonClickCountObserver = new ButtonCountObserver
-//    observableButton.addObserver(buttonClickCountObserver)
-//    for (i <- 1 to 3) observableButton.click()
-//    buttonClickCountObserver.count mustEqual 2
+    val observableButton = new Button("Okay") with ObservableClicks with VetoableClicks {
+      maxAllowed = 2
+    }
+    assert(observableButton.maxAllowed == 2, "observableButton.maxAllowed ==2")
+    val buttonClickCountObserver = new ButtonCountObserver
+    observableButton.addObserver(buttonClickCountObserver)
+    for (i <- 1 to 3)
+      observableButton.click()
+    assert(buttonClickCountObserver.count == 2, "buttonClickCountObserver.count ==2")
   }
 }
