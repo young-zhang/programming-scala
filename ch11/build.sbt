d34f9a03f65879a1b8476010c6c486e5aff2185c
Young Zhang <young.zhang@credit-suisse.com>
2014-01-22 16:28:13 -0500

    Added ch11 payroll-api-script.scala, p219

---
+++ ch11/build.sbt
@@ -0,0 +1,7 @@
+name := "Chapter 11 DSL"
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.0.5"
---
+++ ch11/src/main/scala/payroll/employee.scala
@@ -0,0 +1,7 @@
+// code-examples/DSLs/payroll/employee.scala
+
+package payroll
+
+case class Name(first: String, last: String)
+
+case class Employee(name: Name, annualGrossSalary: Money)
